53b3dd3394d5ec762f993ed634aa48e4be5b8dc5
nagdayaseenimran-blip <nagdayaseenimran@gmail.com>
2026-08-17 04:24:29 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
-main.dart
+import 'package:flutter/material.dart';
+import 'screens/add_transaction_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Khatabook App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AddTransactionScreen(),
+    );
+  }
+}
+
