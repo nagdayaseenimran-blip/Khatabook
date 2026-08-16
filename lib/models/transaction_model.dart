ddb62b2be20d05633eaa0e6c6ddd953aa236e02b
nagdayaseenimran-blip <nagdayaseenimran@gmail.com>
2026-08-17 04:14:57 +0500

    Update transaction_model.dart

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -1,1 +1,14 @@
-lib/models/transaction_model.dart
+import 'package:isar/isar.dart';
+
+part 'transaction_model.g.dart';
+
+@collection
+class TransactionModel {
+  Id id = Isar.autoIncrement;
+
+  late String title;
+  late double amount;
+  late String type; // 'income' ya 'expense'
+  late DateTime date;
+  String? note;
+}
